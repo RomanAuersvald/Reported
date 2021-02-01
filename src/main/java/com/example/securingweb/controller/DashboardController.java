16ab42133ad5e364438e49a353d8a50293781b55
Roman Auersvald <roman.auersvald@ororo.cz>
2021-02-01 19:23:00 +0100

    project progress

--- src/main/java/com/example/securingweb/controller/DashboardController.java
+++ src/main/java/com/example/securingweb/controller/DashboardController.java
@@ -4,6 +4,7 @@ import com.example.securingweb.dao.ProjectRepository;
 import com.example.securingweb.dao.ProjectTaskRepository;
 import com.example.securingweb.dao.UserRepository;
 import com.example.securingweb.model.Project;
+import com.example.securingweb.model.ProjectTask;
 import com.example.securingweb.model.ReportedUser;
 import com.example.securingweb.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,15 +39,28 @@ public class DashboardController {
         Collection<Project> projectList = repository.findProjectsByOwnerId(user.getId());
         model.addAttribute("projects", projectList);
         model.addAttribute("user", user);
+        // seznam projektů a jejich progressu
+        model.addAttribute("projectsProgress", getProjectsProgress(projectList));
         return "/dashboard";
     }
 
-//    private Map<Project, Double> getProjectsProgress(Collection<Project> projects){
-//        Map<Project, Double> progress;
-//        for (){
-//
-//        }
-//    }
+    // vrací seznam projektů a jejich progress v % na základě ukončených tasků
+    private Map<Project, Double> getProjectsProgress(Collection<Project> projects){
+        Map<Project, Double> progress = new HashMap<>();
+        for (Project project: projects){
+            Collection<ProjectTask> projectTasks = taskRepository.findProjectTasksByProjectId(project.getId());
+            Double seconds = 0.0;
+            for(ProjectTask task : projectTasks){
+                if (task.taskComplete()){
+                    seconds += task.getTaskDuration();
+                }
+            }
+            Double hours = seconds / 3600;
+            Double percentDone = (hours /  project.getEstimatedHours()) * 100;
+            progress.put(project, percentDone);
+        }
+        return progress;
+    }
 
 
     private ReportedUser getCurrentLoggedUser(){
