a105dc1508d2e12698d3f33f2ffd44327e861b97
Roman Auersvald <roman.auersvald@ororo.cz>
2021-02-01 16:31:29 +0100

    dash model parametry

--- src/main/java/com/example/securingweb/controller/DashboardController.java
+++ src/main/java/com/example/securingweb/controller/DashboardController.java
@@ -1,15 +1,46 @@
 package com.example.securingweb.controller;
 
+import com.example.securingweb.dao.ProjectRepository;
+import com.example.securingweb.dao.UserRepository;
+import com.example.securingweb.model.Project;
+import com.example.securingweb.model.ReportedUser;
+import com.example.securingweb.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.List;
 
 @Controller
 public class DashboardController {
+
+    @Autowired
+    private ProjectRepository repository;
+
+    @Autowired
+    private ProjectService service;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/dashboard")
-    public String setup() {
+    public String setup(Model model) {
+        ReportedUser user = getCurrentLoggedUser();
+        Collection<Project> projectList = repository.findProjectsByOwnerId(user.getId());
+        model.addAttribute("projects", projectList);
+        model.addAttribute("user", user);
         return "/dashboard";
     }
 
+
+    private ReportedUser getCurrentLoggedUser(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        ReportedUser userDetails = ReportedUser.class.cast(principal);
+        return userRepository.findByUsername(userDetails.getUsername());
+    }
 
 
 }
--- src/main/java/com/example/securingweb/dao/ProjectRepository.java
+++ src/main/java/com/example/securingweb/dao/ProjectRepository.java
@@ -1,15 +1,19 @@
 package com.example.securingweb.dao;
 import com.example.securingweb.model.Project;
+import com.example.securingweb.model.ProjectTask;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface ProjectRepository extends CrudRepository<Project, String> {//MongoRepository<Project, String>{
+public interface ProjectRepository extends CrudRepository<Project, String> {
 
     @Override
     long count();
+
+    Collection<Project> findProjectsByOwnerId(String id);
 }
