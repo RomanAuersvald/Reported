2a653709cfff0d68813ab5a5af702e18a1bf5ae9
FilipRoskot <filip.roskot@gmail.com>
2021-02-02 22:11:57 +0100

    My Account Settings

--- src/main/java/com/example/securingweb/controller/LoginController.java
+++ src/main/java/com/example/securingweb/controller/LoginController.java
@@ -5,9 +5,6 @@ import com.example.securingweb.model.ReportedUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class LoginController {
